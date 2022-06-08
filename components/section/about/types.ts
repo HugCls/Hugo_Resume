c9f4e01a507650ab24909bbe16595826fc7272c1
HugCls <superhugh92@gmail.com>
2022-06-08 13:04:23 +0200

    minor changes

--- components/section/about/types.ts
+++ components/section/about/types.ts
@@ -1,14 +1,8 @@
 export interface AboutInterface {
-  /**
-   * Name of the owner of portfolio.
-   */
+
   name: string
-  /**
-   * About main text: Describe yourself and your work.
-   */
+
   description: string
-  /**
-   * Contact email.
-   */
+
   email: string
 }
