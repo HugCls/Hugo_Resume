a61900f5f2daeec7e688e8c9e0cd9bfc27cf9690
HugCls <superhugh92@gmail.com>
2022-11-19 16:46:08 +0100

    ready to deploy

--- data/en/experiences.ts
+++ data/en/experiences.ts
@@ -8,14 +8,14 @@ const experiences: ExperiencesInterface[] = [
     name: 'Freelance',
     description:
       'I am currently working as a freelance web developer. I am looking for new projects.',
-    expTime: () => getExperienceTime(new Date('2022-05-10')),
+    // expTime: () => getExperienceTime(new Date('2022-05-10')),
     icon: FaDev
   },
   {
     name: 'La Voyette',
     description:
       'Team project for a theater company, making their website from scratch.',
-    expTime: () => getExperienceTime(new Date('2018-10-10')),
+    // expTime: () => getExperienceTime(new Date('2018-10-10')),
     icon: FaTheaterMasks
   },
 
@@ -23,7 +23,7 @@ const experiences: ExperiencesInterface[] = [
     name: 'The Hacking Project',
     description:
       'After six months of intensive hard work and coding, I finally gratuated from the Hacking Project.',
-    expTime: () => getExperienceTime(new Date('2018-10-10')),
+    // expTime: () => getExperienceTime(new Date('2018-10-10')),
     icon: FaUserGraduate
   }
 ]
